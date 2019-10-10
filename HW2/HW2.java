d37344bee12c7756aa9bba2890ac3da559534c04
Aliaksandra.Novikava <darkday97@mail.ru>
2019-10-10 15:22:49 +0300

    HomeWork 2 -Created array with random value -Sorted array

---
+++ HW2/HW2.java
@@ -0,0 +1,37 @@
+public class HW2 {
+
+    public static void main(String[] args){
+        int[] array= new int[10];
+         for(int i= 0; i < array.length; i++)
+         {
+             array[i]= (int) (Math.random()*100)+1;
+         }
+
+        System.out.println("Array without sort:");
+        for(int i= 0; i < array.length; i++)
+        {
+            System.out.print(array[i]+" ");
+        }
+
+boolean isSorted = false;
+int buf;
+        for(int j =0; j< array.length;j++ ) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    buf = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = buf;
+                }
+
+            }
+        }
+        System.out.println();
+        System.out.println("Array after sort:");
+        for(int i= 0; i < array.length; i++)
+        {
+            System.out.print(array[i]+" ");
+        }
+
+
+    }
+}
