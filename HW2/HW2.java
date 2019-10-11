2e9f89360f637792de841606aa14b85cbc79b466
Aliaksandra.Novikava <darkday97@mail.ru>
2019-10-11 11:58:13 +0300

    HomeWork 2 -formatted the code

--- HW2/HW2.java
+++ HW2/HW2.java
@@ -1,40 +1,37 @@
 public class HW2 {
 
-    public static void main(String[] args){
-        int[] array= new int[10];
-         for(int i= 0; i < array.length; i++)
-         {
-             array[i]= (int) (Math.random()*100)+1;
-         }
-
-         int min = array[0];
-         int max =0;
-         double mid =0;
-         int sum= 0;
-
-        for(int i= 0; i < array.length-1; i++)
-        {
-
-            if(min > array[i+1]) {
-           min = array[i+1] ;
-            }
-
-            if (max < array[i]){
-                max= array[i];
-            }
-             sum+= array[i];
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100) + 1;
         }
-         mid= sum/array.length;
+
+        int min = array[0];
+        int max = 0;
+        double mid = 0;
+        int sum = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+            if (min > array[i + 1]) {
+                min = array[i + 1];
+            }
+
+            if (max < array[i]) {
+                max = array[i];
+            }
+            sum += array[i];
+        }
+        mid = sum / array.length;
 
 
         System.out.println("Array without sort:");
-        for(int i= 0; i < array.length; i++)
-        {
-            System.out.print(array[i]+" ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
 
         }
         System.out.println();
-        System.out.println("Min value = "+ min +" Max value = "+ max+ " Average value = " + mid);
+        System.out.println("Min value = " + min + " Max value = " + max + " Average value = " + mid);
         int buf;
         for (int j = 0; j < array.length; j++) {
             for (int i = 0; i < array.length - 1; i++) {
@@ -49,9 +46,8 @@ public class HW2 {
 
         System.out.println();
         System.out.println("Array after sort:");
-        for(int i= 0; i < array.length; i++)
-        {
-            System.out.print(array[i]+" ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
 
 
